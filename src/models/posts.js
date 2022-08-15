be9485c90354b7fae463d1cd94fdc8ed6521759e
sajjad-devsinc <sajjad.mehmood@densinc.com>
2022-08-15 15:27:57 +0500

    updated

--- src/models/posts.js
+++ src/models/posts.js
@@ -7,7 +7,7 @@ const PostSchema = new Schema({
     title: { type: String, required: true, minlength: 4 },
     body: { type: String, required: true, minlength: 8 },
     isPublish: { type: Boolean, required: true },
-    userId: { type: ObjectId, required: true, index: true, ref: 'user' },
+    userId: { type: ObjectId, required: true, index: true },
 })
 
 const PostModel = mongoose.model('post', PostSchema)
--- src/services/posts.js
+++ src/services/posts.js
@@ -1,7 +1,11 @@
 const PostModel = require('../models/posts')
+const ObjectId = require('mongoose').Types.ObjectId
 
-exports.findPost = (params) => PostModel.find(params)
+exports.findPost = (params) => PostModel.find(params).lean()
+
 exports.addPost = (req) => PostModel.create(req.body)
+
 exports.updatePost = (id, req) =>
     PostModel.findByIdAndUpdate({ _id: id }, req.body).lean()
-exports.deletePost = (id) => PostModel.findByIdAndDelete({ _id: id })
+
+exports.deletePost = (id) => PostModel.findByIdAndDelete({ _id: id }).lean()
